897872fc0774938274d8fbf1e2bd1a5f5f7c6624
GuilhermeCardosoSantos <guifoxlokaum@gmail.com>
2025-07-29 17:54:01 -0300

    feat: integrate Header component into Service screen and update Filter navigation

--- src/screens/Filter/Components/Service.tsx
+++ src/screens/Filter/Components/Service.tsx
@@ -1,9 +1,13 @@
 import { View, Text } from "react-native";
+import Header from "~/src/layout/Header";
 
 export default function Service(){
     return(
         <View>
-            <Text>aqui cvhjnfjc</Text>
+            <Header title="Serviço" leftIcon={"back"} />
+            <View className="px-5">
+                <Text>aqui cvhjnfjc</Text>
+            </View>
         </View>
     )
 }
--- src/screens/Filter/Index.tsx
+++ src/screens/Filter/Index.tsx
@@ -70,6 +70,7 @@ export default function Filter() {
         </>
       ) : menu == "service" ? (
         <>
+
           <Service />
         </>
       ) : (
